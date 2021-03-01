4ddf7991711cae057688d6c4258a91d90cf8f18a
marcy3ait <39223917+marcy3ait@users.noreply.github.com>
2021-02-28 22:51:18 -0300

    up

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 
-setup(name='AeroBemt',
-      version='0.1',
-      url='https://github.com/marcy3ait/ProjetoFinal#-triplex-aviation-corp-',
+setup(name='aerobemt',
+      version='1.0',
+      url='https://github.com/marcy3ait/',
       license='MIT',
       author='Triplex Aviation',
       author_email='marcy.borges@unesp.br',
       description='impletação do método BEMT',
-      packages=find_packages(exclude=['tests']),
+      packages=find_packages(),
       long_description=open('README.md', encoding='utf-8').read(),
       zip_safe=False)
